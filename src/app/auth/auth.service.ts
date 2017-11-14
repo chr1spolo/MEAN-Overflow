d4a6fdcbc2c036d839b83a42f59f3389d255bb6a
chr1spolo <polidavis28@gmail.com>
2017-11-13 22:33:31 -0500

    adding user login service

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import urljoin from 'url-join';
+import { environment } from '../../environments/environment';
+import { User } from './user.model';
+import { Http, Headers, Response } from '@angular/http';
+import 'rxjs/Rx';
+import { Resolve } from '@angular/router/src/interfaces';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class AuthService {
+    usersUrl: string;
+    currentUser?: User;
+
+    constructor(private http: Http) {
+        this.usersUrl = urljoin(environment.apiUrl, 'auth');
+        if ( this.userIsLoggedIn() ) {
+            const { userId, firstName, lastName, email } = JSON.parse( localStorage.getItem('user') );
+            this.currentUser = new User(email, null, firstName, lastName, userId);
+        }
+    }
+
+    userSignIn(user: User) {
+        const body = JSON.stringify(user);
+        const headers = new Headers(
+          { 'Content-Type': 'application/json' }
+        );
+        return this.http.post(
+            urljoin(this.usersUrl, 'signin'),
+            body,
+            {headers}
+         ).map( (response: Response) => {
+              const json = response.json();
+              this.userLogIn(json);
+              return json();
+         })._catch( (error: Response) => {
+            console.log(error);
+            return Observable.throw(error.json());
+         } );
+    }
+
+
+    userLogIn = ( { _token, userId, firstName, lastName, email } ) => {
+        this.currentUser = new User(
+            email,
+            null,
+            firstName,
+            lastName,
+            userId
+        );
+        localStorage.setItem('_token', _token);
+        localStorage.setItem('user', JSON.stringify({ userId, firstName, lastName, email }));
+    }
+
+    userIsLoggedIn() {
+        return localStorage.getItem('token') !== null;
+    }
+}
